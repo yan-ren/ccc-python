bf16e49ed9a7984e96a94fbdb36facff7e67a717
Yan Ren <ryan.ren3@gmail.com>
2021-12-19 23:58:45 -0500

    update test runner

--- test_runner.py
+++ test_runner.py
@@ -2,9 +2,10 @@ import subprocess
 from os import listdir
 from os.path import isfile, join
 
-python_version = 'python3'
-test_src = 'src/s2012s1.py'
-test_data_path = 'test/s2012/s1/'
+PYTHON_VERSION = 'python3'
+TEST_SRC = 'src/s2012s1.py'
+TEST_DATA_PATH = 'test/s2012/s1/'
+
 
 def setup(path):
     output = []
@@ -14,26 +15,27 @@ def setup(path):
         output.append((file, file.replace('.in', '.out')))
     return output
 
+
 def run(test_script, input_file):
     with open(input_file, 'r') as f:
         input_value = f.read()
-    result = subprocess.run([python_version, test_script], input=input_value.encode('utf-8'), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    result = subprocess.run([PYTHON_VERSION, test_script], input=input_value.encode('utf-8'), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     return result
 
 
-def compare(result, expected):
+def compare(result, expected, src):
     with open(expected, 'rb') as f:
         expected_result = f.read()
     if result.stderr != b'':
-        print('FAIL: test should not returned stderr:', result.stderr)
+        print('FAIL: test should not returned stderr:', result.stderr, 'src:', src)
     if result.stdout.decode('utf8') == expected_result.decode('utf8'):
         print('PASS', expected)
     else:
-        print('FAIL: expected:', expected_result, ' got:', result.stdout)
+        print('FAIL: expected:', expected_result, ' got:', result.stdout, 'src:', src)
 
 
 if __name__ == '__main__':
-    test_data = setup(test_data_path)
-    for data in test_data:
-        result = run(test_src, data[0])
-        compare(result, data[1])
+    test_data_set = setup(TEST_DATA_PATH)
+    for data in test_data_set:
+        result = run(TEST_SRC, data[0])
+        compare(result, data[1], data[0])
